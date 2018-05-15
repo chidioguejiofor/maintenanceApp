ccbffd72cedb33e0f0640c19cafeca7bbb37fb98
chidioguejiofor@gmail.com <chidioguejiofor@gmail.com>
2018-05-15 17:10:27 +0100

    Adds naviagation through the entire website

--- UI/js/requestPage.js
+++ UI/js/requestPage.js
@@ -1,6 +1,6 @@
 function handleItemClick(event){
     const id = event.target.dataset.id; 
-    window.location.href = `../html/index.html?id=${id}`;
+    window.location.href = `../html/requestStatus.html?id=${id}`;
 
 }
 
