51ee399746cba1ea2c64511122949a7571085442
chidioguejiofor@gmail.com <chidioguejiofor@gmail.com>
2018-05-17 17:16:10 +0100

    adds UserValidator with tests
    A userValidator object is going to be used to check that a User model
    has the valid data
    
    A ModelValidator contains a validate method that return an object
    containing a boolean valid propery

--- server/test/validators/UserValidator.test.js
+++ server/test/validators/UserValidator.test.js
@@ -1,40 +1,46 @@
 /*  eslint no-unused-expressions: off */
 /*  eslint no-undef: off */
 import { expect, assert } from 'chai';
-import Validator from '../../helperModules/ModelValidator';
+import UserValidator from '../../validators/UserValidator';
 import User from '../../models/User';
 
 const goodUser = new User('Chisom', 'password123', 'email@email.com', 'client');
-const goodUserResuult = Validator.validateUser(goodUser);
-const badUserResult1 = Validator.validateUser(new User('123', '', 'email.com', 'chizoba'));
-const badUserTwo = Validator.validateUser(new User('', '123', 'eamil@', 'chef'));
+const badUserOne = new User('123', '', 'email.com', 'chizoba');
+const badUserTwo = new User('', '123', 'eamil@', 'chef');
+
+const validator = new UserValidator(goodUser);
+const goodUserResult = validator.validate();
 
 describe('Testing returned object of validateUser() ', () => {
   describe('If the user is valid then', () => {
-    describe('goodUserResult', () => {
+    describe('value return from validate method', () => {
       it('should be an object', () => {
-        assert(goodUserResuult).isObject();
+        assert.isObject(goodUserResult);
       });
 
       it('should have property valid that is expected to be true', () => {
-        expect(goodUserResuult).property('valid').to.be.true;
+        expect(goodUserResult).property('valid').to.be.true;
+      });
+
+      it('should not contain property email', () => {
+        expect(goodUserResult).not.property('email');
       });
     });
 
     describe('goodUser object', () => {
       it('should have a username property whose length must be greater than 2', () => {
-        expect(goodUser).property('username').to.be.greaterThan(2);
+        expect(goodUser.username.length).to.be.greaterThan(2);
       });
 
       it('should have a email property whose length must be greater than 2', () => {
-        expect(goodUser).property('email').to.be.greaterThan(2);
+        expect(goodUser.email.length).to.be.greaterThan(2);
       });
       it('should have an alphanumeric password property whose length must be greater than 5', () => {
-        expect(goodUser).property('password')
+        expect(goodUser.password.length)
           .to.be.greaterThan(5);
       });
 
-      it('should have an userType  propertythat is either engineer or client', () => {
+      it('should have an userType  property that is either engineer or client', () => {
         expect(goodUser).property('userType');
         const validUsername = goodUser.userType;
         expect(validUsername).to.be.true;
@@ -44,7 +50,7 @@ describe('Testing returned object of validateUser() ', () => {
 
   describe('If user is invalid then', () => {
     it('valid property should be false', () => {
-      expect(badUserResult1).property('valid')
+      expect(badUserOne).property('valid')
         .to.be.false;
       expect(badUserTwo).property('valid')
         .to.be.false;
