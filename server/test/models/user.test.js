0d7188a97d78920c632bd0ff78670811d38f968a
chidioguejiofor@gmail.com <chidioguejiofor@gmail.com>
2018-05-17 13:00:15 +0100

    adds failing tests to the app

--- server/test/models/user.test.js
+++ server/test/models/user.test.js
@@ -1,52 +1,54 @@
+/*  eslint no-unused-expressions: off */
 /*  eslint no-undef: off */
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 import User from '../../models/User';
 
 describe('user', () => {
-  const description = 'The bath thub pipe got broken';
-  const location = 'Top floor of Andela EPIC Tower';
-  const image = 'broken-thub.png';
-  const title = 'Broken pipe';
-  const userType = 'customer';
+  const email = 'test@email.com';
+  const password = 'passs';
+  const username = 'username';
+  const userType = 'super';
 
   let user;
   beforeEach(() => {
-    user = new User(title, description, location, userType, image);
+    user = new User(username, email, password, userType);
   });
 
   describe('constructor', () => {
-
     describe('attributes created in by constructor', () => {
       it('user must be a User', () => {
         expect(user instanceof User).to.be.true;
       });
 
-      it('returned object must have a description attribute', () => {
-        expect(user).to.have.property('description');
+      it('returned object must have a email attribute', () => {
+        expect(user).to.have.property('email');
       });
 
-      it('returned object must have a location attribute', () => {
-        expect(user).to.have.property('location');
+      it('returned object must have a password attribute', () => {
+        expect(user).to.have.property('password');
       });
 
       it('returned object must have a userType attribute', () => {
         expect(user).to.have.property('userType');
       });
-      it('returned object must have a location attribute', () => {
-        expect(user).to.have.property('location');
+      it('returned object must have a username attribute', () => {
+        expect(user).to.have.property('username');
       });
     });
 
     describe('validate attribute values', () => {
-      it(`expect description to equal ${description}`, () => {
-        expect(user).to.have.property('description').to.equal(description);
+      it(`expect email to equal ${email}`, () => {
+        expect(user).to.have.property('email').to.equal(email);
       });
-      it(`expect location to  equal ${location}`, () => {
-        expect(user).to.have.property('location').to.equal(location);
+      it(`expect password to  equal ${password}`, () => {
+        expect(user).to.have.property('password').to.equal(password);
       });
 
       it(`expect userType to equal ${userType}`, () => {
         expect(user).to.have.property('userType').to.equal(userType);
+      });
+      it(`expect userType to equal ${username}`, () => {
+        expect(user).to.have.property('userType').to.equal(username);
       });
     });
   });
